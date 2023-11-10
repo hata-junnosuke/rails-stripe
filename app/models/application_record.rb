85127e968ba53e51520195460f7260045d0d8a1e
hata-junnosuke <jh19911209@icloud.com>
2023-11-10 16:53:08 +0900

    Implementation on the customer's side of the product

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,6 @@
 class ApplicationRecord < ActiveRecord::Base
   primary_abstract_class
+  scope :latest, -> { order(created_at: :desc) }
+  scope :price_high_to_low, -> { order(price: :desc) }
+  scope :price_low_to_high, -> { order(:price) }
 end
